286af0d9fc13d6f140417b09c685aaaa757d8172
John <john@olivetoast.com>
2016-08-01 17:39:51 +0100

    Initial commit. Adding project skeleton and Cartfile (for dependencies)

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# An OpenGL window API implemented in Swift.
+# Built on NanoVG - an OpenGL based vector drawing library.
+
+## Dependencies
+
+1. NanoVG
+   https://github.com/memononen/nanovg
+   
+   Install using the Carthage package management tool:
+   "carthage update"
+   
+   (to install Carthage use Homebrew).
+   
+2. Freetype2
+   https://www.freetype.org/
+   Required for reading ofm fonts (e.g. on macOS)
+   
+
---
+++ Source/Nano.h
@@ -0,0 +1,19 @@
+//
+//  Nano.h
+//  Nano
+//
+//  Created by John on 01/08/2016.
+//  Copyright © 2016 Formal Technology Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Nano.
+FOUNDATION_EXPORT double NanoVersionNumber;
+
+//! Project version string for Nano.
+FOUNDATION_EXPORT const unsigned char NanoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nano/PublicHeader.h>
+
+
